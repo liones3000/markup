a75aa517024b0e15ae0313353ffb8a009ebf7285
liones3000 <liones3000@gmail.com>
2017-08-28 02:15:05 +0300

    50%

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
-$('#form_1').submit(function(event){
+'use strict';
+
+console.clear();
+
+$('body').submit(function(event){
 	event.preventDefault();
+	console.log('stop')
 })
 
 $('#join').on ('mouseover', function(){
@@ -7,4 +12,29 @@ $('#join').on ('mouseover', function(){
 })
 $('#join').on ('mouseout', function(){
 	$(this).find('.arrow').css('color', 'transparent');
+})
+$('#btn-menu').on ('click', function(e){
+	// debugger;
+	e.preventDefault();
+
+	if (e.pageY > 100) {
+		$('#menu').css('opacity', .65);
+	} else {
+		$('#menu').css('opacity', 1);
+	}
+	$('#menu').slideToggle(500);
+});
+
+$(window).scroll(function(e){
+	$('#menu').slideUp(500);
+})
+
+$('#menu').on ('click', 'a.mnu', function(event){
+	event.preventDefault();
+	var id  = $(this).attr('href'),
+	top = $(id).offset().top;
+
+	$('body,html').animate({scrollTop: top}, 1500);
+
+	$('#menu').slideUp(500);
 })
