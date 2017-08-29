9c8b8c01b2cfbe4b60c86b8959737ae11843707d
liones3000 <liones3000@gmail.com>
2017-08-29 18:31:16 +0300

    80%

--- js/script.js
+++ js/script.js
@@ -2,9 +2,8 @@
 
 console.clear();
 
-$('body').submit(function(event){
+$('body').on ('click', 'a', function(event){
 	event.preventDefault();
-	console.log('stop')
 })
 
 $('#join').on ('mouseover', function(){
@@ -13,8 +12,8 @@ $('#join').on ('mouseover', function(){
 $('#join').on ('mouseout', function(){
 	$(this).find('.arrow').css('color', 'transparent');
 })
+
 $('#btn-menu').on ('click', function(e){
-	// debugger;
 	e.preventDefault();
 
 	if (e.pageY > 100) {
@@ -37,4 +36,69 @@ $('#menu').on ('click', 'a.mnu', function(event){
 	$('body,html').animate({scrollTop: top}, 1500);
 
 	$('#menu').slideUp(500);
+});
+
+$('body').on ('submit', function(e){
+	e.preventDefault();
+	$('#openModal').show();
+})
+$('#close').on ('click', function(e){
+	e.preventDefault();
+	$('#openModal').hide();
+});
+
+// Slider
+$('#slideRight').on ('click', function(){
+
+	// TOP
+	$('.triangle-top .tr1, .triangle-top .tr3')
+	.find('span')
+	.html('Lorem ipsum dolor sit <br>amet, consectetur <br>adipisicing elit.<br> Tempore, doloribus!');
+	
+	$('.triangle-top .tr4 .tr-block')
+	.find('span.col-left')
+	.text('Annet,  27')
+	$('.triangle-top .tr5 .tr-block')
+	.find('span.col-left')
+	.text('Doe,  17')
+
+	// BOTTOM
+	$('.triangle-bottom .tr4, .triangle-bottom .tr5')
+	.find('span')
+	.html('Lorem ipsum dolor sit <br>amet, consectetur <br>adipisicing elit.<br> Tempore, doloribus!');
+	
+	$('.triangle-bottom .tr1 .tr-block')
+	.find('span.col-left')
+	.text('Annet,  27');
+
+	$('.triangle-bottom .tr3 .tr-block')
+	.find('span.col-left')
+	.text('Doe,  17');
+})
+$('#slideLeft').on ('click', function(){
+	// TOP
+	$('.triangle-top .tr1, .triangle-top .tr3')
+	.find('span')
+	.html('Сonsectetuer adipiscing <br>elit. Aenean commodo <br>ligula eget dolor. <br>Aenean massa.');
+
+	$('.tr4 .tr-block')
+	.find('span.col-left')
+	.text('Alex,  35');
+
+	$('.tr5 .tr-block')
+	.find('span.col-left')
+	.text('Denis,  28');
+
+	// BOTTOM
+	$('.triangle-bottom .tr4, .triangle-bottom .tr5')
+	.find('span')
+	.html('Сonsectetuer adipiscing <br>elit. Aenean commodo <br>ligula eget dolor. <br>Aenean massa.');
+
+	$('.triangle-bottom .tr1 .tr-block')
+	.find('span.col-left')
+	.text('Anna, 23 ');
+
+	$('.triangle-bottom .tr3 .tr-block')
+	.find('span.col-left')
+	.text('Jane, 27');
 })
